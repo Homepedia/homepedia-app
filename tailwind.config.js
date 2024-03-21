5f005474aceba565d8cb068b410875c88af875e4
Arthur <chaff.arthur@gmail.com>
2024-03-21 10:27:26 +0100

    add hex colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,15 @@ export default {
     ],
     theme: {
         colors: {
-            'primary': '',
+            'primary': '#6057fc',
             'secondary': '',
-            'dark': '',
-            'white': '',
-            'gray': '',
-            'light-gray': '',
-            'dark-gray': '',
-            'success': '',
-            'error': '',
+            'black': '#111a38',
+            'white': '#eaeaea',
+            'gray': '#bdbfc2',
+            'light-gray': '#f6f6f6',
+            'dark-gray': '#4c4c4c',
+            'success': '#10b569',
+            'error': '#d92a2a',
         },
         transitionDuration: {
             '200': '200ms'
