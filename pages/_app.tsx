fb53e7e5b09ac106e13236935252cc771a2b6616
Arthur <chaff.arthur@gmail.com>
2024-07-12 12:04:20 +0200

    add resizable component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from 'next/app';
 import '@/style/globals.css';
 import Header from '@/components/app/header';
+import { memo } from 'react';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
+    const LazyHeader = memo(Header);
+
     return (
-        <main className="h-screen">
-            <Header />
+        <main className="bg-white h-full w-full">
+            <LazyHeader />
             <Component {...pageProps} />
         </main>
     );
--- pages/map.tsx
+++ pages/map.tsx
@@ -1,6 +1,12 @@
 import { Skeleton } from '@/components/ui/skeleton';
 import dynamic from 'next/dynamic';
 
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup
+} from '@/components/ui/resizable';
+
 function LoadingMap() {
     return (
         <Skeleton className="w-full h-full rounded-md" />
@@ -14,8 +20,12 @@ const Map = dynamic(async () => import('@/components/app/map'), {
 
 export default function Dashboard() {
     return (
-        <div className="relative h-[50vh] w-[50vw]">
-            <Map />
+        <div className="relative h-full w-full">
+            <ResizablePanelGroup direction="horizontal">
+                <ResizablePanel defaultSize={5}>One</ResizablePanel>
+                <ResizableHandle withHandle />
+                <ResizablePanel><Map /></ResizablePanel>
+            </ResizablePanelGroup>
         </div>
     );
 }
